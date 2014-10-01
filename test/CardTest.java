37af1e52b5180d9d4651cd6a211a4704996d12c6
Zachary Oscar Ovington <zoo1@pitt.edu>
2014-10-01 10:58:25 -0400

    Periods for everyone

--- test/CardTest.java
+++ test/CardTest.java
@@ -13,65 +13,65 @@ public class CardTest {
 
 Card _card = null; 
 
-//Pass in a value for a card through the constructor and verify it returns that value passed in
+//Pass in a value for a card through the constructor and verify it returns that valid value passed in.
 @Test
 	public void testValueConstructor() {
 		_card = new Card(1,0);
 		assertEquals(_card.getValue(), 1);
 	}
-//Pass in a suit for a card through the constructor and verify it returns the suit passed in
+//Pass in a suit for a card through the constructor and verify it returns the valid suit passed in.
 @Test
 	public void testSuitConstructor() {
 	   _card = new Card(13,3);
 		assertEquals(_card.getSuit(), 3);
 	}
-//Pass in a value for a card through the constructor and verfiy that it returns the correct value as a string(for number cards)
+//Pass in a value for a card through the constructor and verfiy that it returns the correct value as a string(for number cards).
 @Test
 	public void testValueString() {
 	   _card = new Card(5,2);
 		assertEquals(_card.getValueAsString(), "5");
 	}
-//Pass in a value for a card through the constructor and verfiy that it returns the correct value as a string(for royals)
+//Pass in a value for a card through the constructor and verfiy that it returns the correct value as a string(for royals) tested a boundry value.
 @Test
 	public void testValueStringChar() {
 	   _card = new Card(13,1);
 		assertEquals(_card.getValueAsString(), "King");
 	}
 
-//Pass in a suit for a card through the constructor and verfiy that it returns the correct suit as a string
+//Pass in a suit for a card through the constructor and verfiy that it returns the correct suit as a string for a boundry value.
 @Test
 	public void testSuitString() {
 	   _card = new Card(13,3);
 		assertEquals(_card.getSuitAsString(), "Clubs");
 	}
-//Pass in a suit and value and verify the the card is return correctly as a string
+//Pass in a suit and value and verify the the card is return correctly as a string.
 @Test
 	public void testString() {
 	   _card = new Card(11,3);
 		assertEquals(_card.toString(), "Jack of Clubs");
 	}
-//Create a card with an invalid suit and verify the card holds the suit even though it is invalid
+//Create a card with an invalid suit and verify the card holds the suit even though it is invalid. Done on the suites upper boundry.
 @Test
 	public void testInvalidsuit() {
-	   _card = new Card(11,88);
-		assertEquals(_card.getSuit(), 88);
+	   _card = new Card(11,4);
+		assertEquals(_card.getSuit(), 4);
 	}
-//Create a card with an invalid value and verify the card holds the value even though it is invalid
+//Create a card with an invalid value and verify the card holds the value even though it is invalid. Done on the values upper boundry.
 @Test
 	public void testInvalidvalue() {
-	   _card = new Card(2001,0);
-		assertEquals(_card.getValue(), 2001);
+	   _card = new Card(14,0);
+		assertEquals(_card.getValue(), 14);
 	}
-//Create a card with an invalid suit and verify the card returns an invalid string for the suit
+//Create a card with an invalid suit and verify the card returns an invalid string for the suit. Done on the suits lower boundry.
 @Test
 	public void testInvalidsuitstring() {
-	   _card = new Card(11,-55);
+	   _card = new Card(11,-1);
 		assertEquals(_card.getSuitAsString(), "??");
 	}
-//Create a card with an invalid value and verify the card returns an invalid string for the value
+//Create a card with an invalid value and verify the card returns an invalid string for the value. Done on the values lower boundry.
 @Test
 	public void testInvalidvaluestring() {
-	   _card = new Card(-999,2);
+	   _card = new Card(-1,2);
 		assertEquals(_card.getValueAsString(), "??");
 	}
 }
--- test/DeckTest.java
+++ test/DeckTest.java
@@ -9,13 +9,13 @@ import org.junit.Test;
 import org.mockito.MockitoAnnotations;
 public class DeckTest {
 Deck _deck = null; 
-//Verify a created deck has 52 cards if none are used
+//Verify a created deck has 52 cards if none are used.
 @Test
 	public void testInitialCards() {
 		_deck = new Deck();
 		assertEquals(_deck.cardsLeft(), 52);
 	}
-//Verify a created deck has unique cards done through checking that each string isn't equal
+//Verify a created deck has unique cards. Done through checking that each card string isn't equal.
 @Test
 	public void testUniqueDeck() {
 		_deck = new Deck();
@@ -33,7 +33,7 @@ Deck _deck = null;
 		}
 		assertFalse(contain);
 	}
-//Verify the deck will correctly track the ammount of cards that are left in the deck
+//Verify the deck will correctly track the ammount of cards that are left in the deck.
 @Test
 	public void testTrackedammount() {
 		_deck = new Deck();
@@ -43,7 +43,7 @@ Deck _deck = null;
 		}
 		assertEquals(_deck.cardsLeft(),42);
 	}
-//Verify two decks are not the same
+//Verify two decks are not the same after one is shuffled.
 @Test
 	public void testUniqueDecks() {
 		_deck = new Deck();
@@ -57,7 +57,7 @@ Deck _deck = null;
 		}
 		assertFalse(allequal);
 	}
-//Verify no invalid cards are ever dealt by the deck
+//Verify no invalid cards are ever dealt by the deck.
 @Test
 	public void testNoInvalids() {
 		_deck = new Deck();
