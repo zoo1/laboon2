a526496e9396ee3d194e01673e1952d3dc3bfc7f
Zachary Oscar Ovington <zoo1@pitt.edu>
2014-10-01 10:50:50 -0400

    Deck Tests added

--- test/DeckTest.java
+++ test/DeckTest.java
@@ -2,7 +2,71 @@
  *
  * @author Nate Eilert
  */
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
 public class DeckTest {
+Deck _deck = null; 
 //Verify a created deck has 52 cards if none are used
-//
+@Test
+	public void testInitialCards() {
+		_deck = new Deck();
+		assertEquals(_deck.cardsLeft(), 52);
+	}
+//Verify a created deck has unique cards done through checking that each string isn't equal
+@Test
+	public void testUniqueDeck() {
+		_deck = new Deck();
+		String[] cards = new String[52];
+		boolean contain=false;
+		for(int i=0;i<52;i++)
+		{
+			String temp=_deck.dealCard().toString();
+			for(int j=i; j>0;j--)
+			{
+				if(temp.equals(cards[j-1])
+					contain=true;
+			}
+			card[i]=temp;
+		}
+		assertFalse(contain);
+	}
+//Verify the deck will correctly track the ammount of cards that are left in the deck
+@Test
+	public void testTrackedammount() {
+		_deck = new Deck();
+		for(int i=0;i<10;i++)
+		{
+			_deck.dealCard();
+		}
+		assertEquals(_deck.cardsLeft(),42);
+	}
+//Verify two decks are not the same
+@Test
+	public void testUniqueDecks() {
+		_deck = new Deck();
+		_deck2 = new Deck();
+		_deck2.shuffle();
+		boolean allequal=true;
+		for(int i=0;i<52;i++)
+		{
+			if(!(_deck.dealCard().toString().equals(_deck2.dealCard().toString())
+				allequal=false;
+		}
+		assertFalse(allequal);
+	}
+//Verify no invalid cards are ever dealt by the deck
+@Test
+	public void testNoInvalids() {
+		_deck = new Deck();
+		boolean containsinvalid=false;
+		for(int i=0;i<52;i++)
+		{
+			if(_deck.dealCard().toString().contains("?"))
+				containsinvalid=true;
+		}
+		assertFalse(containsinvalid);
+	}
 }
